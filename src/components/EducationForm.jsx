a3ae0a091fa9722f1923cf0036a03c9d4ea8337c
Rob Laughlin <waterfallkicks@gmail.com>
2025-03-17 12:51:15 -0700

    Implement add certification functionality and validation

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/EducationForm.css";
 
+class Certification {
+    constructor(school, fieldOfStudy, completionDate, schoolLocation = "") {
+        this.school = school;
+        this.fieldOfStudy = fieldOfStudy;
+        this.completionDate = completionDate;
+        this.schoolLocation = schoolLocation;
+    }
+}
+
 function EducationForm() {
+    const [certifications, setCertifications] = useState([]);
+    console.log(certifications);
+
+    function addCertificateBtnClicked(e) {
+        // Validate the education subform
+
+        const inputs = Array.from(
+            e.target.parentElement.querySelectorAll("input")
+        );
+
+        inputs.sort((a, b) => {
+            // Sort by priority
+            return a.dataset.priority - b.dataset.priority;
+        });
+
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+            const shouldValidate = input.classList.contains("validate");
+            const isInvalid = !input.reportValidity();
+
+            if (shouldValidate && isInvalid) {
+                // Continue no further if subform is invalid
+                return;
+            }
+        }
+
+        // Form is valid! We can add this certificate
+        setCertifications([
+            {
+                certification: new Certification(...inputs),
+                key: crypto.randomUUID(),
+            },
+            ...certifications,
+        ]);
+    }
+
     return (
         <div className="educationFormContainer formContainer">
             <fieldset className="group">
@@ -16,17 +61,28 @@ function EducationForm() {
                             id="School"
                             className="validate"
                             required
+                            data-priority={1}
                         />
                     </div>
                     <div className="inputWrapper">
                         <label htmlFor="SchoolLocation">SCHOOL LOCATION</label>
-                        <input type="text" id="SchooLocation" />
+                        <input
+                            type="text"
+                            id="SchooLocation"
+                            data-priority={2}
+                        />
                     </div>
                     <div className="inputWrapper fieldOfStudyWrapper">
                         <label htmlFor="FieldOfStudy" className="required">
                             FIELD OF STUDY/CERTIFICATION
                         </label>
-                        <input type="text" id="FieldOfStudy" required />
+                        <input
+                            type="text"
+                            id="FieldOfStudy"
+                            required
+                            className="validate"
+                            data-priority={3}
+                        />
                     </div>
                     <div className="inputWrapper graduationDateWrapper">
                         <label htmlFor="GraduationDate" className="required">
@@ -37,8 +93,16 @@ function EducationForm() {
                             id="GraduationDate"
                             required
                             className="validate"
+                            data-priority={4}
                         />
                     </div>
+                    <button
+                        className="addCertificateBtn"
+                        onClick={addCertificateBtnClicked}
+                        type="button"
+                    >
+                        Add Certificate
+                    </button>
                 </div>
             </fieldset>
         </div>
