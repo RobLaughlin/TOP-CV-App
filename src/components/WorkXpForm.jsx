350481c08930e8b076b4219e138d10ff2e5fa69f
Rob Laughlin <waterfallkicks@gmail.com>
2025-03-19 12:21:17 -0700

    Add work experience subform validation

--- src/components/WorkXpForm.jsx
+++ src/components/WorkXpForm.jsx
@@ -2,6 +2,48 @@ import React from "react";
 import "../styles/WorkXpForm.css";
 
 function WorkXpForm() {
+    function addWorkExperienceBtnClicked(e) {
+        // Validate the work experience subform
+        const grid = e.target.parentElement;
+        const inputs = Array.from(grid.querySelectorAll("input"));
+
+        // Append the textarea input to validate too
+        inputs.push(grid.querySelector("#WorkResponsibilities"));
+
+        inputs.sort((a, b) => {
+            // Sort by priority
+            return a.dataset.priority - b.dataset.priority;
+        });
+
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+            const shouldValidate = input.classList.contains("validate");
+            const isInvalid = !input.reportValidity();
+
+            if (shouldValidate && isInvalid) {
+                // Continue no further if subform is invalid
+                return;
+            }
+        }
+    }
+
+    function validateDates(e) {
+        const grid = e.target.parentElement.parentElement;
+        const workStartDate = grid.querySelector("#WorkStartDate");
+        const workEndDate = grid.querySelector("#WorkEndDate");
+        const startDate = new Date(workStartDate.value);
+        const endDate = new Date(workEndDate.value);
+
+        if (endDate <= startDate) {
+            workEndDate.setCustomValidity(
+                "End date must be set later than the start date"
+            );
+        } else {
+            workEndDate.setCustomValidity("");
+        }
+        workEndDate.reportValidity();
+    }
+
     return (
         <div className="workXpFormContainer formContainer">
             <fieldset className="group">
@@ -41,6 +83,7 @@ function WorkXpForm() {
                             required
                             className="validate"
                             data-priority={3}
+                            onChange={validateDates}
                         />
                     </div>
                     <div className="inputWrapper">
@@ -53,6 +96,7 @@ function WorkXpForm() {
                             required
                             className="validate"
                             data-priority={4}
+                            onChange={validateDates}
                         />
                     </div>
                     <div className="inputWrapper workResponsibilitiesContainer">
@@ -70,7 +114,11 @@ function WorkXpForm() {
                             data-priority={5}
                         ></textarea>
                     </div>
-                    <button className="addExperienceBtn" type="button">
+                    <button
+                        className="addExperienceBtn"
+                        type="button"
+                        onClick={addWorkExperienceBtnClicked}
+                    >
                         Add Work Experience
                     </button>
                 </div>
