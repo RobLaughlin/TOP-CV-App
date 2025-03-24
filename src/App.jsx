09312782bbae76d379587418eb75d2a358f8d0c3
Rob Laughlin <waterfallkicks@gmail.com>
2025-03-24 15:33:40 -0700

    Implement basic build resume button

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,21 @@ import EducationForm from "./components/EducationForm";
 import WorkXpForm from "./components/WorkXpForm";
 import SkillsForm from "./components/SkillsForm";
 
+import { IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
 function App() {
     const [selected, setSelected] = useState(0);
     const [workExperiences, setWorkExperiences] = useState([]);
     const [certifications, setCertifications] = useState([]);
     const [skills, setSkills] = useState([]);
+    const [submittedHeaders, setSubmittedHeaders] = useState(new Map());
+
+    console.log(workExperiences);
+    console.log(certifications);
+    console.log(skills);
+    console.log(submittedHeaders);
+
     const RESUME_SECTIONS = [
         ["Heading", <HeadingForm />],
         [
@@ -30,6 +40,7 @@ function App() {
         ],
         ["Skills", <SkillsForm skills={skills} setSkills={setSkills} />],
     ];
+
     function validateHeader(forceSwitch = false) {
         const headerForm = document.querySelector(".headingFormContainer");
         const inputs = Array.from(headerForm.querySelectorAll("input, select"));
@@ -55,6 +66,14 @@ function App() {
         }
 
         return true;
+    }
+
+    function formatDate(dateString) {
+        return new Date(dateString)
+            .toDateString()
+            .split(" ")
+            .slice(1)
+            .join(" ");
     }
 
     // Validate the header after tabs are changed
@@ -84,7 +103,14 @@ function App() {
             ])
         );
 
-        console.log(headerFields);
+        setSubmittedHeaders(headerFields);
+        const resumeContainer = document.querySelector(
+            ".finishedResumeContainer"
+        );
+
+        resumeContainer.classList.remove("slideOut");
+        resumeContainer.classList.remove("invisible");
+        resumeContainer.classList.add("slideIn");
     }
 
     function itemClicked(e) {
@@ -94,44 +120,146 @@ function App() {
         }
     }
 
+    function closeBtnClicked() {
+        const finishedResume = document.querySelector(
+            ".finishedResumeContainer"
+        );
+
+        finishedResume.classList.remove("slideIn");
+        finishedResume.classList.add("slideOut");
+        finishedResume.classList.add("invisible");
+    }
+
     return (
-        <div className="appContainer">
-            <div className="sectionContainer">
-                <header>
-                    <h1 className="title">Build a Resume!</h1>
-                </header>
-                <Switch
-                    items={RESUME_SECTIONS.map((item) => {
-                        return item[0];
-                    })}
-                    itemClicked={itemClicked}
-                    selected={selected}
-                />
-                <button
-                    className="generateResumeBtn"
-                    onClick={buildResumeBtnClicked}
-                >
-                    Build Resume
-                </button>
+        <>
+            <div className="appContainer">
+                <div className="sectionContainer">
+                    <header>
+                        <h1 className="title">Build a Resume!</h1>
+                    </header>
+                    <Switch
+                        items={RESUME_SECTIONS.map((item) => {
+                            return item[0];
+                        })}
+                        itemClicked={itemClicked}
+                        selected={selected}
+                    />
+                    <button
+                        className="generateResumeBtn"
+                        onClick={buildResumeBtnClicked}
+                    >
+                        Build Resume
+                    </button>
+                </div>
+                <div className="panelContainer">
+                    <form
+                        className="resumeForm"
+                        autoComplete="off"
+                        id="ResumeForm"
+                    >
+                        {RESUME_SECTIONS.map((item, idx) => {
+                            const [_, form] = item;
+                            const display = idx === selected ? "block" : "none";
+                            return (
+                                <div
+                                    className="panel"
+                                    style={{ display }}
+                                    key={idx}
+                                >
+                                    {form}
+                                </div>
+                            );
+                        })}
+                    </form>
+                </div>
             </div>
-            <div className="panelContainer">
-                <form className="resumeForm" autoComplete="off" id="ResumeForm">
-                    {RESUME_SECTIONS.map((item, idx) => {
-                        const [_, form] = item;
-                        const display = idx === selected ? "block" : "none";
-                        return (
-                            <div
-                                className="panel"
-                                style={{ display }}
-                                key={idx}
-                            >
-                                {form}
-                            </div>
-                        );
-                    })}
-                </form>
+            <div className="finishedResumeContainer">
+                <div className="body">
+                    <IconButton
+                        aria-label="close"
+                        className="closeBtn"
+                        size="medium"
+                        onClick={closeBtnClicked}
+                    >
+                        <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                    <div className="finishedHeaderContainer container">
+                        <h1>
+                            {submittedHeaders.get("FirstName")}{" "}
+                            {submittedHeaders.get("LastName")}
+                        </h1>
+                        <h3>
+                            {submittedHeaders.get("EmailAddress")} | +1{" "}
+                            {submittedHeaders.get("PhoneNumber")}
+                        </h3>
+                        <h3>
+                            {submittedHeaders.get("City")},{" "}
+                            {submittedHeaders.get("State")}{" "}
+                            {submittedHeaders.get("Zip")}
+                        </h3>
+                    </div>
+                    <hr />
+                    <div className="finsihedEducationContainer container">
+                        <h1>Education</h1>
+                        <ul>
+                            {certifications.map(({ certification, key }) => {
+                                return (
+                                    <li key={key}>
+                                        <h3>
+                                            {certification.fieldOfStudy} |{" "}
+                                            {formatDate(
+                                                certification.completionDate
+                                            )}
+                                        </h3>
+                                        <h4>
+                                            {certification.school}
+                                            {certification.schoolLocation !== ""
+                                                ? " | " +
+                                                  certification.schoolLocation
+                                                : ""}
+                                        </h4>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <hr />
+                    <div className="finishedWorkXpContainer container">
+                        <h1>Work Experience</h1>
+                        <ul>
+                            {workExperiences.map(({ xp, key }) => {
+                                return (
+                                    <li key={key}>
+                                        <h3>
+                                            {xp.company} | {xp.location}
+                                        </h3>
+                                        <h3>{xp.jobTitle}</h3>
+                                        <h4>
+                                            From {xp.startDate} to {xp.endDate}
+                                        </h4>
+                                        <p>{xp.responsibilities}</p>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <hr />
+                    <div className="finishedSkillsContainer container">
+                        <h1>Skills</h1>
+                        <ul>
+                            {skills.map(({ skill, key }) => {
+                                return (
+                                    <li key={key}>
+                                        <b>{skill.name}</b> -{" "}
+                                        {skill.description}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
